abbe77fc699265da67b1d741a85e0e5b1dd2cc0b
dheap3 <davidjamesheap@gmail.com>
2025-03-04 22:54:13 -0700

    reorganize components(gameplay holds all play html and handles game funcitonality)

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import "./play.css";
 
-const KeyboardResponsive = () => {
+const GamePlay = () => {
   const [keyPressed, setNewCommand] = useState('Command');
   const [bgColor, setColor] = useState({style: {bgColor: "hsl(0, 100%, 75%)"}});
   const [animateText, setAnimateText] = useState(false);
@@ -23,58 +23,39 @@ const KeyboardResponsive = () => {
       return () => window.removeEventListener('keydown', handleKeyDown);
   }, []);
   return (
-    <motion.div
-    initial={{ y: 0 }}
-    animate={
-      animateText ?
-        keyPressed === 'ArrowUp' || keyPressed === 'w' ? { y: -101, opacity: 0 } : 
-        keyPressed === 'ArrowDown' || keyPressed === 's' ? { y: 101, opacity: 0 } : 
-        keyPressed === 'ArrowLeft' || keyPressed === 'a' ? { x: -101, opacity: 0 } : 
-        keyPressed === 'ArrowRight' || keyPressed === 'd' ? { x: 101, opacity: 0 } : { y: 1, opacity: 1 }
-      : { y: 1, opacity: 1 }
-    }
-    transition={{ duration: 0.2 }}
+    <div id="gameplaycomponent">
+      <div className="goal" id="top"></div>
+      <div id="inline">
+        <div className="goal" id="left"></div>
+        
+        {/* command in the middle, moving around */}
+        <motion.div
+        initial={{ y: 0 }}
+        animate={
+          animateText ?
+            keyPressed === 'ArrowUp' || keyPressed === 'w' ? { y: -101, opacity: 0 } : 
+            keyPressed === 'ArrowDown' || keyPressed === 's' ? { y: 101, opacity: 0 } : 
+            keyPressed === 'ArrowLeft' || keyPressed === 'a' ? { x: -101, opacity: 0 } : 
+            keyPressed === 'ArrowRight' || keyPressed === 'd' ? { x: 101, opacity: 0 } : { y: 1, opacity: 1 }
+          : { y: 1, opacity: 1 }
+        }
+        transition={{ duration: 0.2 }}
 
-    onAnimationComplete={() => { console.log('Animation Completed'); setAnimateText(false); }}
-    style={{color: bgColor}}>Command</motion.div>
-    // <motion.div
-    //   initial={{ y: 0 }}
-    //   animate={
-    //     animateText
-    //       ? keyPressed === 'ArrowUp'
-    //         ? { y: -100, opacity: 0 }
-    //         : keyPressed === 'ArrowDown'
-    //         ? { y: 100, opacity: 0 }
-    //         : keyPressed === 'ArrowLeft'
-    //         ? { x: -100, opacity: 0 }
-    //         : keyPressed === 'ArrowRight'
-    //         ? { x: 100, opacity: 0 }
-    //         : { y: 0, opacity: 1 }
-    //       : { y: 0, opacity: 1 } // Move to y: 0 after initial animation
-    //   }
-    //   transition={{ duration: 0.5 }}
-    //   onAnimationComplete={() => {
-    //     console.log('First animation complete, triggering next animation');
-    //     setAnimateText(false); // After the first animation, move to y: 200
-    //   }}
-    //   style={{ color: bgColor }}
-    // >
-    //   Command {keyPressed}
-    // </motion.div>
+        onAnimationComplete={() => { setAnimateText(false); }}
+        style={{color: bgColor}}
+        id="command">Command</motion.div>
+
+        <div className="goal" id="right"></div>
+      </div>
+      <div className="goal" id ="bottom"></div>
+    </div>
   );
 };
 
 export function Play() {
   return (
     <main>
-      <div className="goal" id="top"></div>
-      <div id="inline">
-        <div className="goal" id="left"></div>
-        <div id="command"><KeyboardResponsive /></div>
-        <div className="goal" id="right"></div>
-      </div>
-      <div className="goal" id ="bottom"></div>
-
+      <GamePlay />
       <p>API notifications of people passing you on the leaderboard go here</p>
     </main>
   );
