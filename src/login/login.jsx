9d39cddd9dfb51dd3cb91956353d2c2a41448fc1
dheap3 <davidjamesheap@gmail.com>
2025-03-08 13:15:25 -0700

    login works with master login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./login.css";
+import { useNavigate } from 'react-router-dom';
+
+let allUserInfo = [
+  { username: "masteruser", password: "masterpassword" }
+];
+let verified = false;
+
+const verifyUser = (username, password) => {//userInfo = [{ username: "qwerty", password: "qwerty" }]
+  console.log(allUserInfo);
+  for (let i = 0; i < allUserInfo.length; i++) {
+    if (username === allUserInfo[i].username && password === allUserInfo[i].password) {
+      verified = true;
+    }
+  }
+};
 
 export function Login() {
-  const displayUser = () => {
-    console.log("User logged in!");
-  };
+  const [userName, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+  const proceedToPlay = () => {
+    if (verified) {
+      navigate('/play');
+    } else {
+      console.log("User not found, please create an account and then login.");
+    }
+  }
+
   return (
     <main>
         <div id="login">
             <p>Login to Mind Boggle</p>
-            <form onClick={displayUser}>
-              <input placeholder="your@email.com" />
-              <input placeholder="password" />
-              <button>Login</button>
+            <form id ="loginForm"> {/* not called in the css, but fixes an error in the chrome compiler */}
+              <input placeholder="username" onChange={(e) => setUserName(e.target.value)}/>
+              <input placeholder="password" type ="password" onChange={(e) => setPassword(e.target.value)}/>
+              <button type="button" onClick={() => {verifyUser(userName, password); proceedToPlay()}}>Login</button>
               <button>Create</button>
             </form>
           </div>
